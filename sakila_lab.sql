81040416f4c333fd6e91a6c4194b72e8c2cbae48
mrtimN <t.niewolinski@gmail.com>
2025-01-28 15:09:59 +0100

    adjustments

--- sakila_lab.sql
+++ sakila_lab.sql
@@ -13,17 +13,13 @@ select release_year from film group by release_year;
 
 select count(store_id) as number_of_stores from store;
 select count(staff_id) as number_of_employees from staff;
-select count(inventory_id) as available_movies from rental where date_format(rental_date, '%d%') - date_format(return_date, '%d%') > 0;
-select count(inventory_id) as rented_movies from rental where date_format(rental_date, '%d%') - date_format(return_date, '%d%') < 0;
-
+select count(inventory_id) as available_movies from rental where return_date is not null;
+select count(inventory_id) as rented_movies from rental where return_date is null;
 select distinct count(last_name) as distinct_last_name from actor;
 
-select title, length
-from film
-order by length desc
-limit 10;
+select title from film order by length desc limit 10;
 
 select * from actor where first_name='SCARLETT';
 
-select * from film where title like '%ARMAGEDDON%' having length > 100;
+select * from film where title like '%ARMAGEDDON%' and length > 100;
 select count(film_id) from film where special_features like '%Behind the Scenes%';
